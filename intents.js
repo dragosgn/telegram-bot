1d6da70270798d6fb32a1312547162faf1348a6b
Dragos <dragosgn@gmail.com>
2017-10-09 09:44:19 +0200

    added some intetns

---
+++ intents.js
@@ -0,0 +1,27 @@
+{"intents": [
+  {
+    "tag": "greeting",
+    "patterns": ["Hi", "How are you?", "Is anyone there?", "Hello", "Good day!"],
+    "responses": ["Hello, thanks for visiting", "Good to see you again", "Hi there, how can I help?"],
+    "context_set": ""
+  },
+  {
+    "tag": "goodbye",
+    "patterns": ["Bye", "See you later", "Goodbye"],
+    "responses": ["See you later, tank you for visiting", "Have a nice day", "Bye! Come back again soon"],
+    "context_set": ""
+  },
+  {
+    "tag": "thanks",
+    "patterns": ["Hi", "How are you?", "Is anyone there?", "Hello", "Good day!"],
+    "responses": ["Hello, thanks for visiting", "Good to see you again", "Hi there, how can I help?"],
+    "context_set": ""
+  },
+  {
+    "tag": "hours",
+    "patterns": ["Hi", "How are you?", "Is anyone there?", "Hello", "Good day!"],
+    "responses": ["Hello, thanks for visiting", "Good to see you again", "Hi there, how can I help?"],
+    "context_set": ""
+  }
+]
+}
